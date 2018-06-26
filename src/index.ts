364878f08ffc6f240c76e5c638b111ab9441d26a
problem233 <problem233@outlook.com>
2018-06-27 00:10:47 +0800

    edit

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 'use strict'
 import { JSDOM } from 'jsdom'
 
-import { isDirectory } from './util'
-
 export const sampleCrawler = () => JSDOM
   .fromURL("https://github.com/jsdom/jsdom")
   .then(dom => dom.window.document)
   .then(document => console.log(document
     .getElementsByClassName("repository-meta-content")[0]
     .innerHTML))
-
-if (isDirectory('./private'))
-  module.exports.private = require('./private')
--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,10 @@
 'use strict'
 import * as fs from 'fs'
 
+/*
 export const isDirectory = (path: string) =>
   fs.existsSync(path) && fs.lstatSync(path).isDirectory()
+*/
 
 export const withLog = message => <T>(value: T) => {
   console.log(message)
